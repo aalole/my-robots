c999e7eb76f8351066403e34295551747a2a7d69
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-08-19 12:52:01 +0100

    created build veesion of my robots

---
+++ precache-manifest.1855139dbddefcf8969c2e4de6a6bc52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8ae85ff89d48d257ea74f824d39c09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36f27e34a4b95a988132",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5b4f9c4fe819f78c2d69",
+    "url": "/static/css/main.ab76ffed.chunk.css"
+  },
+  {
+    "revision": "36f27e34a4b95a988132",
+    "url": "/static/js/2.15838daa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15838daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4f9c4fe819f78c2d69",
+    "url": "/static/js/main.bcd10be5.chunk.js"
+  },
+  {
+    "revision": "3a3b720ac7166cde82fa",
+    "url": "/static/js/runtime-main.ff1f797a.js"
+  }
+]);
